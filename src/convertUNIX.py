746493ea3b7872874c1fda190428f2ab92e336f4
edel <edel.altares@gmail.com>
2018-03-18 22:12:22 -0600

    Added timezone changes and time validating

--- src/convertUNIX.py
+++ src/convertUNIX.py
@@ -1,6 +1,48 @@
-from datetime import datetime
+import datetime
 import argparse
 import sys
+
+def valid_date(date):
+  six = datetime.time(6, 0, 0, 0)
+  midnight = datetime.time.max
+
+  new_date = datetime.datetime.time(date)
+  
+  ONT_START = datetime.datetime(2017, 9, 15)
+  ONT_END = datetime.datetime(2017, 9, 17)
+
+
+  if ((new_date > six and new_date < midnight) and
+    (date > ONT_END or date < ONT_START)):
+    return True
+  
+  return False
+
+
+def time_zone(date):
+  new_date = date
+
+  # Philippines trips
+  MNL_TRIP_1_START = datetime.datetime(2013, 5, 9)
+  MNL_TRIP_1_END = datetime.datetime(2013, 5, 29)
+
+  MNL_TRIP_2_START = datetime.datetime(2015, 7, 5)
+  MNL_TRIP_2_END = datetime.datetime(2013, 8, 7)
+
+  # Waterloo trips
+  ONT_START = datetime.datetime(2017, 9, 15)
+  ONT_END = datetime.datetime(2017, 9, 17)
+
+  # if certain date, change to philippines time
+  if ((date > MNL_TRIP_1_START and date < MNL_TRIP_1_END) or
+      (date > MNL_TRIP_2_START and date < MNL_TRIP_2_END)):
+    new_date = date + datetime.timedelta(hours=12)
+
+  # if certain date, change to ontario time
+  elif date > ONT_START and date < ONT_END:
+    new_date = date + datetime.timedelta(hours=2)
+
+  return new_date
 
 def parse_csv(filename):
   content = ""
@@ -17,18 +59,28 @@ def parse_csv(filename):
     new_line = ""
 
     try:
-      new_date = datetime.fromtimestamp(float(int(date)))
+
+      new_date = datetime.datetime.fromtimestamp(float(int(date)))
+
+      new_date = time_zone(new_date)
+
+      if not valid_date(new_date):
+        print("INVALID")
+        print(new_date)
+        continue
+
       new_date = new_date.strftime('%Y-%m-%d %H:%M:%S')
       
       new_line += new_date
       new_line += "\t".join(line[1:])
 
-      print(new_line)
 
-    except IndexError:
-      sys.stderr.write(str(len(line)))
-    except Exception:
-      sys.stderr.write(date)
+    except IndexError as i:
+      print(str(len(line)))
+      print(i)
+    except Exception as e:
+      print(date)
+      print(e)
       
 parser = argparse.ArgumentParser()
 parser.add_argument('filename')
