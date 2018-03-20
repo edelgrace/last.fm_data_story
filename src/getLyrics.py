98aac357b8eed2b89ffcfa1c0ecadff26ad9db9f
edel <edel.altares@gmail.com>
2018-03-19 19:54:18 -0600

    Lyrics set generated

--- src/getLyrics.py
+++ src/getLyrics.py
@@ -1,48 +1,52 @@
 import lyricsgenius as genius
 import argparse
+import json
 
 def getSongInfo(song):
-  data = song.strip().split("\t")
+  data = json.loads(song)
 
-  title = data[1]
-  artist = data[2]
+  title = data["Song_Title"]
+  artist = data["Artist"]
 
   return title, artist
 
 def getLyrics(filename, token):
-  try:
+  song_list = []
 
-    # initialize api
-    api = genius.Genius(token)
+  # initialize api
+  api = genius.Genius(token)
 
-    # open the file
-    file = open(filename, mode='r')
-    contents = file.read()
-    songs = contents.split("\n")
-
-    # create a new file
-    old_filename = filename.split(".csv")
-    new_filename = old_filename[0] + "_lyrics.csv"
-    new_file = open(new_filename, mode='a')
-
-    new_contents = ""
-
-    # loop through each song
-    for song in songs:
-      title, artist = getSongInfo(song)
+  # open the file
+  file = open(filename, mode='r')
+  contents = file.read()
+  songs = contents.split("\n")
   
-      result = api.search_song(title, artist)
-      
-      if not result is None:
-        lyrics = result.lyrics
-        lyrics = lyrics.replace("\n","\\")
+  # create a new file
+  old_filename = filename.split(".csv")
+  new_filename = old_filename[0] + "_lyrics.csv"
+  new_file = open(new_filename, mode='a')
 
-        new_line = title + "\t" + artist + "\t\"" + lyrics + "\"\n"
+  # loop through each song
+  for song in songs:
+    title, artist = getSongInfo(song)
 
-      else:
-        new_line = title + "\t" + artist + "\t\n"
-  
-      new_file.write(new_line)
+    if (title, artist) in song_list:
+      continue
+
+    song_list.append((title, artist))
+
+    result = api.search_song(title, artist)
+    
+    if not result is None:
+      lyrics = result.lyrics
+      lyrics = lyrics.replace("\n","\\")
+
+      new_line = title + "\t" + artist + "\t\"" + lyrics + "\"\n"
+
+    else:
+      new_line = title + "\t" + artist + "\t\n"
+
+    new_file.write(new_line)
 
   return
 
